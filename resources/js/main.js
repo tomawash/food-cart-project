fc6d3578d7a5e9a0e554ceb50a9fd5f5ff78cb4d
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-03-22 21:33:59 -0400

    Add google maps to foodcarts
    foodcarts.html
    - Added google maps div and function set up for each food cart
    - Displays all maps on page load
    
    * Added various comments for page sections

---
+++ resources/js/main.js
@@ -0,0 +1,30 @@
+/**
+ * Google Maps init function
+ * To add more maps, modify the following "googleMap" function and call it
+ */
+function initMap() {
+    googleMap1();
+    googleMap2();
+    googleMap3();
+}
+
+function googleMap1() {
+    let NY = { lat: 40.631096, lng: -73.952426 };
+    let map = new google.maps.Map(
+        document.getElementById('googleMap1'), { zoom: 16.5, center: NY });
+    let marker = new google.maps.Marker({ position: NY, map: map });
+}
+
+function googleMap2() {
+    let NY = { lat: 40.631096, lng: -73.952426 };
+    let map = new google.maps.Map(
+        document.getElementById('googleMap2'), { zoom: 16.5, center: NY });
+    let marker = new google.maps.Marker({ position: NY, map: map });
+}
+
+function googleMap3() {
+    let NY = { lat: 40.631096, lng: -73.952426 };
+    let map = new google.maps.Map(
+        document.getElementById('googleMap3'), { zoom: 16.5, center: NY });
+    let marker = new google.maps.Marker({ position: NY, map: map });
+}
