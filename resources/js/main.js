03f407fac4019d6172f41ecc6ee0d494af8b8bdb
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-03-23 00:47:10 -0400

    Added slider button feedback for open foodcarts

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,7 @@
+$(document).ready(() => {
+    document.getElementById("sliderInfo").innerHTML = "OFF";
+})
+
 /**
  * Google Maps init function
  * To add more maps, modify the following "googleMap" function and call it
@@ -27,4 +31,15 @@ function googleMap3() {
     let map = new google.maps.Map(
         document.getElementById('googleMap3'), { zoom: 16.5, center: NY });
     let marker = new google.maps.Marker({ position: NY, map: map });
+}
+
+let sliderBool = 0;
+function switchSlider() {
+    if (sliderBool === 0) {
+        document.getElementById("sliderInfo").innerHTML = "ON";
+        sliderBool = 1;
+    } else {
+        document.getElementById("sliderInfo").innerHTML = "OFF";
+        sliderBool = 0;
+    }
 }
