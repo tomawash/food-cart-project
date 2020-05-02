4ceefa7ef8500741ddf69044afaae28ceeb3f140
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-05-02 16:18:54 -0400

    Change google map initialization for better readability
    Additionally, after an individual clicks the search button, feedback is provided.

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,38 +2,53 @@ $(document).ready(() => {
     document.getElementById("sliderInfo").innerHTML = "OFF";
 })
 
+const foodCarts = [
+    {
+        id: 1,
+        location: {
+            latitude: 40.631096,
+            longitude: -73.952426
+        }
+    },
+    {
+        id: 2,
+        location: {
+            latitude: 40.631096,
+            longitude: -73.952426
+        }
+    },
+    {
+        id: 3,
+        location: {
+            latitude: 40.631096,
+            longitude: -73.952426
+        }
+    }
+]
+
 /**
  * Google Maps init function
- * To add more maps, modify the following "googleMap" function and call it
+ * To add more maps, modify the array of food cart objects above and add html
+ * With real implementation of back-end the array isn't initialized here
  */
 function initMap() {
-    googleMap1();
-    googleMap2();
-    googleMap3();
+    for (let i=0; i<foodCarts.length; i++) {
+        googleMap(foodCarts[i], foodCarts[i].id);
+    }
 }
 
-function googleMap1() {
-    let NY = { lat: 40.631096, lng: -73.952426 };
+function googleMap(foodCart, index) {
+    let NY = { lat: foodCart.location.latitude, lng: foodCart.location.longitude };
+    let id = "googleMap" + index;
     let map = new google.maps.Map(
-        document.getElementById('googleMap1'), { zoom: 16.5, center: NY });
-    let marker = new google.maps.Marker({ position: NY, map: map });
-}
-
-function googleMap2() {
-    let NY = { lat: 40.631096, lng: -73.952426 };
-    let map = new google.maps.Map(
-        document.getElementById('googleMap2'), { zoom: 16.5, center: NY });
-    let marker = new google.maps.Marker({ position: NY, map: map });
-}
-
-function googleMap3() {
-    let NY = { lat: 40.631096, lng: -73.952426 };
-    let map = new google.maps.Map(
-        document.getElementById('googleMap3'), { zoom: 16.5, center: NY });
+        document.getElementById(id), { zoom: 16.5, center: NY });
     let marker = new google.maps.Marker({ position: NY, map: map });
 }
 
 let sliderBool = 0;
+/**
+ * Function for displaying "on" or "off" for the foodcart on/off toggle
+ */
 function switchSlider() {
     if (sliderBool === 0) {
         document.getElementById("sliderInfo").innerHTML = "ON";
@@ -42,4 +57,10 @@ function switchSlider() {
         document.getElementById("sliderInfo").innerHTML = "OFF";
         sliderBool = 0;
     }
+}
+
+function onSearch() {
+    let searchResults = document.getElementById("search").value;
+    document.getElementById("searchResults").style.color = "green";
+    document.getElementById("searchResults").innerHTML = "Searched for: " + searchResults;
 }
